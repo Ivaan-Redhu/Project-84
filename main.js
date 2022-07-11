aa5804175eaab1c70bab22b6761d00df973e349a
Ivaan-Redhu <93902463+Ivaan-Redhu@users.noreply.github.com>
2022-07-11 19:16:28 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -31,50 +31,51 @@ function my_keydown(e) {
 	if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90))
 	//write a code to check the type of key pressed
 	{
-		otherkey();
-		document.getElementById("d1").innerHTML = "You pressed symbol or other key";
-		console.log("other key")
-	}
-}
-
-function aplhabetkey() {
-	//upload respective image with the message. 
-	keyPressed = e.keyCode;
-	console.log(keyPressed);
-
-	if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90))
-	//write a code to check the type of key pressed
-	{
 		alphabetkey();
 		document.getElementById("d1").innerHTML = "You pressed Alphabet Key";
 		console.log("alphabet key")
 	}
-}
-function numberkey() {
-	keyPressed = e.keyCode;
-	console.log(keyPressed);
-
-	if (keyPressed >= 48 && keyPressed <= 57)
-	//write a code to check the type of key pressed
-	{
+	else if (keyPressed >= 48 && keyPressed <= 57) {
 		numberkey();
 		document.getElementById("d1").innerHTML = "You pressed Number Key";
-		console.log("number key")
+		console.log("Number key");
 	}
-	function specialkey() {
-		keyPressed = e.keyCode;
-		console.log(keyPressed);
+	else if (keyPressed >= 37 && keyPressed <= 40) {
+		arrowkey();
+		document.getElementById("d1").innerHTML = "You pressed Arrow Key";
+		console.log("Arrow Key");
+	}
+	else if ((keyPressed == 17) || (keyPressed == 18 || keyPressed == 27)) {
+		specialkey();
+		document.getElementById("d1").innerHTML = "You pressed Special Key";
+		console.log("Special key");
+	}
+	else {
+		otherkey();
+		document.getElementById("d1").innerHTML = "You pressed Other Key";
+		console.log("Other Key");
+	}
+}
 
-		if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90))
-		//write a code to check the type of key pressed
-		{
-			otherkey();
-			document.getElementById("d1").innerHTML = "You pressed symbol or other key";
-			console.log("alphabet key")
-		}
-	}
-	function otherkey() {
-		img_image = "otherkey.png";
-		add();
-	}
+function alphabetkey() {
+	//upload respective image with the message. 
+	img_image = "Alpkey.png";
+	add();
+}
 
+function numberkey() {
+	img_image = "numkey.png";
+	add();
+}
+function specialkey() {
+	img_image = "spkey.png";
+	add();
+}
+function otherkey() {
+	img_image = "otherkey.png";
+	add();
+}
+function arrowkey() {
+	img_image = "Arrkey.png";
+	add();
+}
